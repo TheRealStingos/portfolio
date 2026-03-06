e164b332259625479397813fa53bfd86246d35e9
Stingos <Justmo10@gmail.com>
2026-03-06 13:36:52 -0500

    add basic badge styling

--- src/components/Badges.tsx
+++ src/components/Badges.tsx
@@ -1,9 +1,8 @@
-import { FaAws, FaCloudflare, FaCss3, FaDocker, FaGithub, FaGitSquare, FaHtml5, FaJs, FaNode, FaPython, FaReact } from "react-icons/fa";
+import { FaAws, FaCloudflare, FaCss3, FaCss3Alt, FaDocker, FaGithub, FaGitSquare, FaHtml5, FaJs, FaJsSquare, FaNode, FaPython, FaReact } from "react-icons/fa";
 import Skills from "./Skills";
+import { SiCss, SiExpress, SiFastapi, SiHtml5, SiJavascript, SiJest, SiNextdotjs, SiNginxproxymanager, SiPostgresql, SiReact, SiSqlalchemy, SiTailwindcss, SiTypescript, SiVitest } from "react-icons/si";
+import { FaLinux} from "react-icons/fa6";
 import { BsTypescript } from "react-icons/bs";
-import { SiExpress, SiFastapi, SiJest, SiNginxproxymanager, SiPostgresql, SiSqlalchemy, SiTailwindcss, SiVitest } from "react-icons/si";
-import { FaLinux } from "react-icons/fa6";
-import { RiNextjsFill } from "react-icons/ri";
 
 export default function Badges() {
     return (
@@ -12,13 +11,13 @@ export default function Badges() {
             <article className="flex flex-col items-center">
                 <h1>Front End</h1>
                 <div className="grid grid-cols-3">
-                    <Skills logo={FaHtml5} title="HTML" description={null} variant="badge" />
-                    <Skills logo={FaCss3} title="CSS" description={null} variant="badge" />
-                    <Skills logo={FaJs} title="JavaScipt" description={null} variant="badge" />
+                    <Skills logo={FaHtml5} title="HTML5" description={null} variant="badge" />
+                    <Skills logo={FaCss3Alt} title="CSS3" description={null} variant="badge" />
+                    <Skills logo={FaJsSquare} title="JavaScipt" description={null} variant="badge" />
                     <Skills logo={BsTypescript} title="TypeScript" description={null} variant="badge" />
                     <Skills logo={SiTailwindcss} title="Tailwind" description={null} variant="badge" />
                     <Skills logo={FaReact} title="React" description={null} variant="badge" />
-                    <Skills logo={RiNextjsFill} title="Nextjs" description={null} variant="badge" />
+                    <Skills logo={SiNextdotjs} title="Nextjs" description={null} variant="badge" />
                 </div>
             </article>
 
@@ -26,11 +25,11 @@ export default function Badges() {
             <article className="flex flex-col items-center">
                 <h1>Back End</h1>
                 <div className="grid grid-cols-2">
-                    <Skills logo={FaPython} title="Python" description={null} variant="badge" />
-                    <Skills logo={SiFastapi} title="FastAPI" description={null} variant="badge" />
                     <Skills logo={FaNode} title="Node" description={null} variant="badge" />
                     <Skills logo={SiExpress} title="Express" description={null} variant="badge" />
-                    <Skills logo={SiSqlalchemy} title="SQLAlchemy" description={null} variant="badge" />
+                    <Skills logo={FaPython} title="Python" description={null} variant="badge" />
+                    <Skills logo={SiFastapi} title="FastAPI" description={null} variant="badge" />
+                    <Skills logo={SiSqlalchemy} title="" description={null} variant="badge" iconSize={60}/>
                     <Skills logo={SiPostgresql} title="PostgreSQL" description={null} variant="badge" />
                 </div>
             </article>
@@ -44,9 +43,9 @@ export default function Badges() {
                     <Skills logo={FaGithub} title="Github" description={null} variant="badge" />
                     <Skills logo={FaLinux} title="Linux" description={null} variant="badge" />
                     <Skills logo={FaDocker} title="Docker" description={null} variant="badge" />
-                    <Skills logo={SiNginxproxymanager} title="Nginx Proxy" description={null} variant="badge" />
+                    <Skills logo={SiNginxproxymanager} title="Nginx PM" description={null} variant="badge" />
                     <Skills logo={FaCloudflare} title="Cloudflare" description={null} variant="badge" />
-                    <Skills logo={FaAws} title="AWS" description={null} variant="badge" />
+                    <Skills logo={FaAws} title="" description={null} variant="badge" />
                 </div>
             </article>
         </section>
--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -5,14 +5,17 @@ interface Skills{
     title: string
     description: string | null
     variant?: "card" | "badge"
+    iconSize?: number
 }
 
-export default function Skills({ logo: Icon, title, description, variant }: Skills) {
+export default function Skills({ logo: Icon, title, description, variant = "card", iconSize = 32}: Skills) {
     if (variant === "badge") {
         return (
-            <div className="flex items-center justify-center gap-1 bg-amber-200 rounded-2xl p-3 m-2 h-12">
-                <Icon size={32} />
-                <h3>{title}</h3>
+            <div className="flex items-center justify-center gap-2 bg-black rounded-2xl m-2 p-2 h-12 w-32 transition-transform hover:scale-110 cursor-pointer duration-75 hover:invert">
+                <div className=" flex items-center justify-center">
+                    <Icon size={iconSize} />
+                </div>
+                <h3 className="text-sm">{title}</h3>
             </div>
         )
     }
