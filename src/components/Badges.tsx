ecf8419b99b49ee1b2bff3ead2295628b82cb437
Stingos <Justmo10@gmail.com>
2026-03-06 12:28:02 -0500

    add badge section

--- src/components/Badges.tsx
+++ src/components/Badges.tsx
@@ -1,10 +1,54 @@
-import { FaJs } from "react-icons/fa";
+import { FaAws, FaCloudflare, FaCss3, FaDocker, FaGithub, FaGitSquare, FaHtml5, FaJs, FaNode, FaPython, FaReact } from "react-icons/fa";
 import Skills from "./Skills";
+import { BsTypescript } from "react-icons/bs";
+import { SiExpress, SiFastapi, SiJest, SiNginxproxymanager, SiPostgresql, SiSqlalchemy, SiTailwindcss, SiVitest } from "react-icons/si";
+import { FaLinux } from "react-icons/fa6";
+import { RiNextjsFill } from "react-icons/ri";
 
 export default function Badges() {
     return (
-        <div>
-            <Skills logo={FaJs} title="JavaScipt" description={null} variant="badge" />
-        </div>
+        <section className="flex gap-20 items-center justify-center">
+            {/*Front End*/}
+            <article className="flex flex-col items-center">
+                <h1>Front End</h1>
+                <div className="grid grid-cols-3">
+                    <Skills logo={FaHtml5} title="HTML" description={null} variant="badge" />
+                    <Skills logo={FaCss3} title="CSS" description={null} variant="badge" />
+                    <Skills logo={FaJs} title="JavaScipt" description={null} variant="badge" />
+                    <Skills logo={BsTypescript} title="TypeScript" description={null} variant="badge" />
+                    <Skills logo={SiTailwindcss} title="Tailwind" description={null} variant="badge" />
+                    <Skills logo={FaReact} title="React" description={null} variant="badge" />
+                    <Skills logo={RiNextjsFill} title="Nextjs" description={null} variant="badge" />
+                </div>
+            </article>
+
+            {/*Back End*/}
+            <article className="flex flex-col items-center">
+                <h1>Back End</h1>
+                <div className="grid grid-cols-2">
+                    <Skills logo={FaPython} title="Python" description={null} variant="badge" />
+                    <Skills logo={SiFastapi} title="FastAPI" description={null} variant="badge" />
+                    <Skills logo={FaNode} title="Node" description={null} variant="badge" />
+                    <Skills logo={SiExpress} title="Express" description={null} variant="badge" />
+                    <Skills logo={SiSqlalchemy} title="SQLAlchemy" description={null} variant="badge" />
+                    <Skills logo={SiPostgresql} title="PostgreSQL" description={null} variant="badge" />
+                </div>
+            </article>
+            
+            {/*Tools*/}
+            <article className="flex flex-col items-center">
+                <h1>Tooling & Infrastructure</h1>
+                <div className="grid grid-cols-3">
+                    <Skills logo={SiJest} title="Jest" description={null} variant="badge" />
+                    <Skills logo={SiVitest} title="Vitest" description={null} variant="badge" />
+                    <Skills logo={FaGithub} title="Github" description={null} variant="badge" />
+                    <Skills logo={FaLinux} title="Linux" description={null} variant="badge" />
+                    <Skills logo={FaDocker} title="Docker" description={null} variant="badge" />
+                    <Skills logo={SiNginxproxymanager} title="Nginx Proxy" description={null} variant="badge" />
+                    <Skills logo={FaCloudflare} title="Cloudflare" description={null} variant="badge" />
+                    <Skills logo={FaAws} title="AWS" description={null} variant="badge" />
+                </div>
+            </article>
+        </section>
     )
 }
--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -10,7 +10,7 @@ interface Skills{
 export default function Skills({ logo: Icon, title, description, variant }: Skills) {
     if (variant === "badge") {
         return (
-            <div className="flex items-center gap-1">
+            <div className="flex items-center justify-center gap-1 bg-amber-200 rounded-2xl p-3 m-2 h-12">
                 <Icon size={32} />
                 <h3>{title}</h3>
             </div>
