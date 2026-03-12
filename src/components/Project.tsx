55d81fa77fe68459028c78e0f3eb46787aec3ba8
Stingos <Justmo10@gmail.com>
2026-03-12 13:36:18 -0400

    finish project section layout and styling

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -12,17 +12,19 @@ interface Project {
 
 export default function Project({ screenshot, title, description, repo, demo }: Project) {
   return (
-    <div className="flex flex-col items-center m-4">
-        <div className="flex flex-col items-center">
-        <h1 className="text-2xl">{title}</h1>
-        <Image src={screenshot} alt={title} />
-        <div className="flex flex-col items-center">
-            <p>{description}</p>
-            <div className="flex gap-8 items-center">
-                <a href={repo}><FaGithub size={32} className="hover:invert hover:cursor-pointer transition-transform hover:scale-110 duration-75" /></a>
-                {demo && <a href={demo} className="bg-amber-950 rounded-md p-1 hover:invert hover:cursor-pointer transition-transform hover:scale-105 duration-75">Try it out!</a>}
+    <div className="flex m-auto space-x-72 items-center">
+        <div className="flex flex-col w-72 h-60 p-2 bg-white border border-slate-200 rounded-xl shadow-sm hover:shadow-md hover:shadow-blue-100 transition-all duration-200 hover:-translate-y-1">
+            <div className="flex flex-col">
+            <h1 className="text-2xl text-slate-900 text-center mt-8">{title}</h1>
+            <p className="text-slate-600 mt-4 text-center">{description}</p>
+            <div className="flex gap-8 items-center justify-center mt-8">
+                <a href={repo}><FaGithub size={32} color="#3b82f6" className="hover:invert hover:cursor-pointer transition-transform hover:scale-110 duration-75 " /></a>
+                {demo && <a href={demo} className="bg-blue-500 text-white px-4 py-2 rounded-lg transition-transform hover:scale-110 duration-75 hover:invert cursor-pointer">Try it out!</a>}
+            </div>
             </div>
         </div>
+        <div className="flex flex-col items-center bg-white border border-slate-200 shadow-sm rounded-xl p-3 m-6">
+            <Image src={screenshot} alt={title} height={288} width={512} className="rounded-2xl"/>
         </div>
     </div>
   )
--- src/components/ProjectSection.tsx
+++ src/components/ProjectSection.tsx
@@ -1,14 +1,15 @@
 import Project from "./Project"
 import placeholder from "../assets/image-placeholder-icon-15-1769662654.png"
+import memory from "../assets/memoryScreen.png"
 
 
 export default function ProjectsSection() {
     return(
-        <section id="projectsSection" className="grid grid-cols-2">
-            <Project screenshot={placeholder} title="Media Server" description="Home Media Server with containerized structure, " repo="" />
-            <Project screenshot={placeholder} title="Memory Game" description="Card matching memory game built with React" repo="Code: " demo="https://memory.justinmoore.dev/" />
-            <Project screenshot={placeholder} title="JMOFlix" description="" repo="" />
-            <Project screenshot={placeholder} title="WGUPS" description="" repo="" />
+        <section id="projectsSection" className="flex flex-col">
+            <Project screenshot={placeholder} title="Media Server" description="Home Media Server with containerized structure and automated media management " repo="" />
+            <Project screenshot={memory} title="Deadlock Memory Game" description="Card matching memory game built with React with a Deadlock theme" repo="Code: " demo="https://memory.justinmoore.dev/" />
+            <Project screenshot={placeholder} title="JMOFlix" description="ML movie recommendation system using TF vectorization" repo="" />
+            <Project screenshot={placeholder} title="WGUPS" description="Mileage calculator with custom built algorithmic solution" repo="" />
         </section>
     )
 }
