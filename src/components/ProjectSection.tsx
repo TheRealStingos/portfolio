d027fa486cef6af2f2b14124ea6c92ff62cc28d7
Stingos <Justmo10@gmail.com>
2026-04-03 14:14:46 -0400

    fix jumble project component

--- src/components/ProjectSection.tsx
+++ src/components/ProjectSection.tsx
@@ -8,7 +8,7 @@ import jumble from "../assets/jumbleScreen.png"
 export default function ProjectsSection() {
     return(
         <section id="projectsSection" className="flex flex-col m-24">
-            <Project screenshot={jumble} title="Jubmle" description="All in One Media Logging Social Media App! Log, rate, and review your favorite movies, video games, music, and books. Compare with your friends! *Under Construction*"
+            <Project screenshot={jumble} title="Jubmle" description="All in One Media Logging Social Media App! Log, rate, and review your favorite movies, video games, music, and books. Compare with your friends! *Under Construction*" repo="https://github.com/TheRealStingos/jumble" demo="https://jumble.justinmoore.dev"/>
             <Project screenshot={media} title="Media Server" description="Home Media Server with containerized structure and automated media management. It's even hosting this portfolio!" repo="https://github.com/TheRealStingos/media-server" />
             <Project screenshot={memory} title="Deadlock Memory Game" description="Card matching memory game built with React with a Deadlock theme" repo="Code: " demo="https://memory.justinmoore.dev/" />
             <Project screenshot={movie} title="JMOFlix" description="ML movie recommendation system using TF vectorization" repo="https://github.com/TheRealStingos/WGU-Capstone" />
